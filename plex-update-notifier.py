6c72097ca258c17d4de4e88ecc1d6a27e99f2e0f
Greg Baker <gregspammagnet@gmail.com>
2016-04-05 06:52:25 +0000

    Added pip requirements file. Updated plex-update-notifier.py to loop through servers and check if they're available.

--- plex-update-notifier.py
+++ plex-update-notifier.py
@@ -2,6 +2,7 @@ import requests
 import json
 import getpass
 import xml.etree.ElementTree as ET
+import logging
 
 class Plex:
     """A class for managing a small subset of Plex API calls"""
@@ -22,6 +23,7 @@ class Plex:
     def servers(self):
         url = ''.join([self.api_url, '/pms/servers'])
         r = requests.get(url, headers=self.headers)
+        logger.debug(r.text)
         root = ET.fromstring(r.text)
         servers = [] 
         for child in root:
@@ -37,12 +39,43 @@ class Plex:
         body = r.json()
         return len(body['_children']) > 0
 
+    def server_available(self, address, port):
+        url = "http://{0}:{1}/identity".format(address, port)
+        try:
+            r = requests.get(url)
+            return r.status_code == 200
+        except:
+            return False
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
 
 if __name__ == "__main__":
+    logger.info('Starting script')
     user = raw_input('Username: ') 
     password = getpass.getpass()
+    logger.info('Creating Plex object')
     plex = Plex(user, password, 'Plex Update Notifier')
+    logger.info('Getting list of plex servers')
     servers = plex.servers()
+    logger.debug(servers)
     for server in servers:
-        print "{0} Update Available?: {1}".format(server["name"],
-                                                  plex.update_available(server['address'], server['port'])) 
+        server['online'] = plex.server_available(server['address'], server['port'])
+
+        if server['online']:
+            server['update_available'] = plex.update_available(server['address'], server['port'])
+        else:
+            server['update_available'] = None
+
+    print "{0} Update Available?: {1}".format(server["name"], server["update_available"])
+
+
